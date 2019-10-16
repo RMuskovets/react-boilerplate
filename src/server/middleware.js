70e08ecf2f400ac57290618ff430ea4cdfd91ff6
RMuskovets <rmuskovets@gmail.com>
2019-10-16 20:19:40 +0300

    add http auth middleware

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -6,6 +6,31 @@
 
 module.exports = {
 	middlewares: {
-		plain: (req, res, next) => { next() }
+		plain: (req, res, next) => { next() },
+
+		basicDictAuth: (userDictionary) => (req, res, next) => {
+			let header = req.get('Authorization');
+			let base64 = Buffer.from(header, 'base64').toString('utf8');
+			let splitd = base64.split(':');
+			let [ username, password ] = splitd;
+
+			let auth = false;
+
+			if (Object.keys(userDictionary).indexOf(username) != -1
+			 && Object.values(userDictionary).indexOf(password) != -1) auth = true;
+
+			if (auth) next();
+			else res.sendStatus(403);
+		},
+		basicFnAuth: (fn) => (req, res, next) => {
+			let header = req.get('Authorization');
+			let base64 = Buffer.from(header, 'base64').toString('utf8');
+			let splitd = base64.split(':');
+			let [ username, password ] = splitd;
+
+			let auth = fn(username, password);
+			if (auth) next();
+			else res.sendStatus(403);
+		}
 	}
 }
